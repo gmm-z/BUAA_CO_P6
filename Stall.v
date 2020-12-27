4e69ee530bcfa9acc9b49a0ee5c655238082f471
Aurora <2732834341@qq.com>
2020-12-27 17:26:38 +0800

    更改了Tuse，默认为3，且beq，jr的Tuse为0

--- Stall.v
+++ Stall.v
@@ -63,14 +63,11 @@ module Stall(
 		assign ID_Tnew_i = (D_addu || D_subu || D_ori || D_lui || D_jal)? 1:
 									(D_lw ) ? 2: 0;
 		
-		assign Tuse_rs = (D_addu || D_subu || D_ori || D_lw || D_sw) ? 1 : 0;
-		assign Tuse_rt = (D_addu || D_subu) ? 1 : 
-								(D_sw )? 2 : 0;
-							
-		//MEM_Tnew_o
-		//EX_Tnew_o
-		//input [4:0]MEM_RegAddr_o,
-		//input [4:0]EX_RegAddr_o,
+		assign Tuse_rs = (D_beq || D_jr) ? 0:(D_addu || D_subu || D_ori || D_lw || D_sw) ? 1 : 3;
+		assign Tuse_rt =  (D_beq) ? 0: 
+								(D_addu || D_subu) ? 1 : 
+								(D_sw )? 2 : 3;
+
 		wire stall_rs,stall_rt;
 		assign stall_rs = ( (EX_RegAddr_o == D_rs && EX_RegAddr_o != 0 && Tuse_rs < EX_Tnew_o ) || (MEM_RegAddr_o == D_rs && MEM_RegAddr_o !=0 && Tuse_rs < MEM_Tnew_o) )? 1 : 0; 
 		assign stall_rt =( (EX_RegAddr_o == D_rt && EX_RegAddr_o != 0 && Tuse_rt < EX_Tnew_o ) || (MEM_RegAddr_o == D_rt && MEM_RegAddr_o !=0 && Tuse_rt < MEM_Tnew_o) )? 1 : 0; 
